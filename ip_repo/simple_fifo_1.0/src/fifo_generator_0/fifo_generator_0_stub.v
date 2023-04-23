767d6da8ce14c33f97ae078802bede14faf9d0a7
Khai <kchang3970@gmail.com>
2023-04-23 19:23:39 -0400

    Add simple fifo

---
+++ ip_repo/simple_fifo_1.0/src/fifo_generator_0/fifo_generator_0_stub.v
@@ -0,0 +1,29 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+// Date        : Sun Apr 23 16:17:53 2023
+// Host        : MasterPC running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/zybo_project/Lab7/radio_periph_lab/ip_repo/simple_fifo_1.0/src/fifo_generator_0/fifo_generator_0_stub.v
+// Design      : fifo_generator_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_2_7,Vivado 2022.1" *)
+module fifo_generator_0(clk, srst, din, wr_en, rd_en, dout, full, empty, 
+  data_count)
+/* synthesis syn_black_box black_box_pad_pin="clk,srst,din[31:0],wr_en,rd_en,dout[31:0],full,empty,data_count[15:0]" */;
+  input clk;
+  input srst;
+  input [31:0]din;
+  input wr_en;
+  input rd_en;
+  output [31:0]dout;
+  output full;
+  output empty;
+  output [15:0]data_count;
+endmodule
